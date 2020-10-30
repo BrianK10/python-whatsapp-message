6b85465ed35c992e8de4d21acfcc57eb57b9252e
Brian Kahn <bkahn.james@gmail.com>
2020-10-30 00:33:34 -0500

    Bluce for

--- whatsapp_message.py
+++ whatsapp_message.py
@@ -4,10 +4,15 @@ import pywhatkit as wsp
 #Variables de celular + mensaje 
 phone = "+51980309999"
 message = "Hola Brian, este es un mensaje indicando que ya puedes utilizar tu tarjeta virtual Edenred."
-
+cantidad = 1
+minute = 26
 try:
-	wsp.sendwhatmsg(phone,message, 23,45)
-	print("Enviado")
+
+	while cantidad <= 10:
+		wsp.sendwhatmsg(phone,message, 00,minute + 1,5)
+		print("Enviado a " + phone)
+		cantidad +=1
+		minute +=1
 except:
 	print("Error al enviar mensaje :(")
 	
