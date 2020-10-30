ee3cd40ba41c22e81c5bee37f115ca2fd95f67e8
Brian Kahn <bkahn.james@gmail.com>
2020-10-29 23:53:58 -0500

    Comentarios

--- whatsapp_message.py
+++ whatsapp_message.py
@@ -1,6 +1,10 @@
+#Uso de libreria pywhatkit
 import pywhatkit as wsp
+
+#Variables de celular + mensaje 
 phone = "+51980309999"
 message = "Hola Brian, este es un mensaje indicando que ya puedes utilizar tu tarjeta virtual Edenred."
+
 try:
 	wsp.sendwhatmsg(phone,message, 23,45)
 	print("Enviado")
